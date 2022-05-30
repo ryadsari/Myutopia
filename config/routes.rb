f355d43297268b444263741d0998e602075b5e81
Gordana Marjanovic <gogamar@gmail.com>
2022-05-30 15:38:43 +0200

    added models, controllers and initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
+  resources :characters
+  resources :worlds
 end
