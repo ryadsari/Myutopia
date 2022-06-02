6b22bc073d9c6ac35fefdba78a1840f2592919b1
Gordana Marjanovic <gogamar@gmail.com>
2022-06-02 12:16:13 +0200

    seed

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,7 @@
 class Character < ApplicationRecord
   belongs_to :user
-  has_many :chatrooms
+  has_many :chatrooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
   has_one_attached :photo
   validates :name, presence: true, length: { minimum: 6 }
 end
